8ed9aa9ebe5f086ef8cfc2581f35801c0cc7f7d8
Maia.dev <asmodeoanxy@gmail.com>
2025-09-02 21:50:55 -0300

    Código roto...

--- Assets/Scripts/TiendaIntento/PlayerShop.cs
+++ Assets/Scripts/TiendaIntento/PlayerShop.cs
@@ -1,14 +1,53 @@
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class PlayerShop : MonoBehaviour
 {
     private int money = 500;
     [SerializeField] private Shop shop;
-    private SimpleList<ItemSO> list;
+
+    [SerializeField] ItemListSO allItems;
+    private SimpleList<GameObject> playerItems;
+    private Dictionary<GameObject, int> itemQuantities;
+    [SerializeField] GameObject itemPrefab;
 
     void Start()
     {
-        list = new SimpleList<ItemSO>();
+        itemQuantities = new Dictionary<GameObject, int>();
+
+        for (int i = 0; i < allItems.items.Length; i++)
+        {
+            GameObject newItem = itemPrefab;
+            ItemSO item = allItems.items[i];
+            newItem.GetComponent<Image>().sprite = item.Sprite;
+            Transform hijo = newItem.transform.Find("NameText");
+
+            if (hijo != null)
+            {
+                TextMeshProUGUI texto = hijo.GetComponent<TextMeshProUGUI>();
+                if (texto != null)
+                {
+                    texto.text = allItems.items[i].name;
+                }
+            }
+            Transform hijo2 = newItem.transform.Find("PriceText");
+            if (hijo != null)
+            {
+                TextMeshProUGUI texto = hijo.GetComponent<TextMeshProUGUI>();
+                if (texto != null)
+                {
+                    texto.text = allItems.items[i].ItemPrice.ToString();
+                }
+            }
+            GameObject instantiatedItem = Instantiate(newItem, transform);
+            //instantiatedItem.GetComponent<Button>().onClick.RemoveAllListeners();
+           // instantiatedItem.GetComponent<Button>().onClick.AddListener(() => playerShop.BuyItem(item));
+           instantiatedItem.SetActive(false);
+           playerItems.Add(instantiatedItem);
+        }
     }
     void Update()
     {
@@ -22,18 +61,21 @@ public class PlayerShop : MonoBehaviour
             if(itemSO1 != null)
             {
                 Debug.Log("Compre: " +  itemSO1.name);
-                list.Add(itemSO1);
+                itemQuantities[itemSO.ID] +=1;
                 UpdateUI();
                 money -= itemSO.ItemPrice;
                 Debug.Log(money.ToString());
             }
         }
     }
-
     private void UpdateUI()
     {
-
+        foreach (var kvp in itemQuantities)
+        {
+            if(kvp.Value > 0)
+            {
+                allItems.items[kvp.Key].gameObject.SetActive(true);
+            }
+        }
     }
-
-
 }
