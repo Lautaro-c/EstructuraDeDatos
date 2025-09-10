efa6aee67ba142f5bc3acb3e81c0717adc8c1bdd
Maia.dev <asmodeoanxy@gmail.com>
2025-09-09 21:32:34 -0300

    Recursiones hechas, falta UI

---
+++ Assets/Scripts/TP9/Recursion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Recursion : MonoBehaviour
+{
+    private int[] numsArray;
+
+    public int Sum(int[] nums, int currentIndex)
+    {
+        //caso recursivo
+        if (currentIndex < numsArray.Length - 1)
+        {
+            return nums[currentIndex] + Sum(nums, currentIndex + 1);
+        }
+
+
+        return nums[currentIndex];
+    }
+
+    public int Fibonacci(int num)
+    {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+
+        //caso recursivo
+        return Fibonacci(num - 1) + Fibonacci(num - 2);
+    }
+
+    public int Factorial(int num)
+    {
+        if (num == 0 || num == 1) return 1;
+
+        //caso recursivo
+        else return num * Factorial(num - 1);
+    }
+
+    public string Pyramid(int maxHeight, int currentHeight = 0)
+    {
+
+        if (currentHeight == maxHeight)
+            return "";
+
+        int spaces = maxHeight - currentHeight - 1;
+        int xCount = 2 * currentHeight + 1;
+
+        string line = new string(' ', spaces) + new string('x', xCount) + "\n";
+
+        //caso recursivo
+        return line + Pyramid(maxHeight, currentHeight + 1);
+    }
+    public bool Palíndromo (string palabra)
+    {
+        // Caso base
+        if (palabra.Length <= 1)
+            return true;
+
+        // Caso base
+        if (palabra[0] != palabra[palabra.Length - 1])
+            return false;
+
+        // Llamada recursiva
+        else return Palíndromo(palabra.Substring(1, palabra.Length - 2));
+    }
+}
---
+++ Assets/Scripts/TP9/SceneRecursive.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneRecursive : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] Recursion recursion;
+
+    private void Start()
+    {
+        inputField = FindAnyObjectByType<TMP_InputField>();
+        recursion = FindAnyObjectByType<Recursion>();
+    }
+
+    public void UseFibonacci()
+    {
+        if (int.TryParse(inputField.text, out int maxNum))
+        {
+            text.text = "El golden ratio es de";
+            int i = 0;
+            int fib = recursion.Fibonacci(i);
+            while (fib <= maxNum)
+            {
+                text.text += " " + fib + ", ";
+                i++;
+                fib = recursion.Fibonacci(i);
+            }
+        }
+        else
+        {
+            text.text = "Ingrese un número válido";
+        }
+
+    }
+    public void UseFactorial()
+    {
+        if (int.TryParse(inputField.text, out int num))
+        {
+            int factorial = recursion.Factorial(num);
+            text.text = "El factorial de " + num + " es " + factorial;
+        }
+        else
+        {
+            text.text = "Ingrese un número válido";
+        }
+    }
+    public void ClearInputF()
+    {
+        inputField.text = "";
+    }    
+}
