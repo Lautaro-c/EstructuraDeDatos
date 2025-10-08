d7c8f8da2539e9565d52fbb1b7d274a217787cfd
Maia.dev <asmodeoanxy@gmail.com>
2025-10-08 10:59:59 -0300

    Anda todo lol

--- Assets/Grupo 03/TP 07/Scripts/LeaderboardManager.cs
+++ Assets/Grupo 03/TP 07/Scripts/LeaderboardManager.cs
@@ -18,27 +18,28 @@ public class LeaderboardManager : MonoBehaviour
             int score = UnityEngine.Random.Range(0, 1000);
             scoreTree.Insert(new ScoreEntry(name, score));
         }
-        UpdateUI();
+        UpdateUI(scoreTree.InOrderTraversal());
+
     }
 
     public void AddNewScore(string name, int score)
     {
         scoreTree.Insert(new ScoreEntry(name, score));
-        UpdateUI();
+        UpdateUI(scoreTree.InOrderTraversal());
     }
 
-    void UpdateUI()
+    void UpdateUI(List<ScoreEntry> scores)
     {
         foreach (Transform child in contentPanel)
             Destroy(child.gameObject);
 
-        List<ScoreEntry> scores = scoreTree.InOrderTraversal(); // Implementar en AVL
         foreach (var entry in scores)
         {
             GameObject item = Instantiate(scorePrefab, contentPanel);
             item.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = entry.ToString();
         }
     }
+
     public void AddScoreInput()
     {
         string playerName = inputName.text;
@@ -53,5 +54,43 @@ public class LeaderboardManager : MonoBehaviour
             Debug.LogWarning("Puntaje inválido");
         }
     }
+    public void LevelOrder()
+    {
+        var nodes = scoreTree.LevelOrder();  // SimpleList<Node<ScoreEntry>>
+        List<ScoreEntry> scores = ConvertToList(nodes);
+        UpdateUI(scores);
+    }
+
+    public void PreOrder()
+    {
+        var nodes = scoreTree.PreOrder();
+        List<ScoreEntry> scores = ConvertToList(nodes);
+        UpdateUI(scores);
+    }
+
+    public void PostOrder()
+    {
+        var nodes = scoreTree.PostOrder();
+        List<ScoreEntry> scores = ConvertToList(nodes);
+        UpdateUI(scores);
+    }
+
+    public void InOrder()
+    {
+        var nodes = scoreTree.InOrder();
+        List<ScoreEntry> scores = ConvertToList(nodes);
+        UpdateUI(scores);
+    }
+    public List<ScoreEntry> ConvertToList(SimpleList<Node<ScoreEntry>> nodeList)
+    {
+        List<ScoreEntry> list = new List<ScoreEntry>();
+        for (int i = 0; i < nodeList.Count; i++)
+        {
+            list.Add(nodeList[i].Data);
+        }
+        return list;
+    }
+
+
 
 }
